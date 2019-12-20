cafcf7643aaec8f6eb4c94a1e39ba8a34c87c819
Max Ruckriegel <maxr@zhinst.com>
2019-12-20 13:27:55 +0100

    add Waveform class
    - takes in two arrays/lists
    - holds parameters like buffer length
    - makes sure waveforms aligned to 16 samples
    - interleaves waveforms
    - handles alignment to start/end within granularity
    - w.data holds array ready to be sent to AWG

--- devices.py
+++ devices.py
@@ -214,13 +214,74 @@ class UHFQA(ZiDevice):
 
 class AWG(object):
     def __init__(self):
-        pass
+        self.in_use = False
+        self.__sequence = SequenceProgram()
+        self.awg = None  # daq.awgModule()
+        self.waveforms = []
 
-    def update(self, settings):
+    def set(self, **settings):
+        self.__sequence.set(**settings)
+
+    def set_waveform(self, wave1, wave2):
         raise NotImplementedError
 
     def upload_waveform(self):
         raise NotImplementedError
+
+
+class Waveform(object):
+    def __init__(self, wave1, wave2, granularity=16, align_start=True):
+        self.granularity = granularity
+        self.align_start = align_start
+        self.buffer_length = self.round_up(max(len(wave1), len(wave2), 16))
+        self.data = self.interleave_waveforms(wave1, wave2)
+
+    def interleave_waveforms(self, x1, x2):
+        # if waveforms are empty, upload zeros
+        if len(x1) == 0:
+            x1 = np.zeros(1)
+        if len(x2) == 0:
+            x2 = np.zeros(1)
+
+        n = max(len(x1), len(x2))
+        n = min(n, self.buffer_length)
+        m1, m2 = np.max(np.abs(x1)), np.max(np.abs(x2))
+        data = np.zeros((2, self.buffer_length))
+
+        if self.align_start:
+            # align to start
+            if len(x1) > n:
+                data[0, :n] = x1[:n] / m1 if m1 >= 1 else x1[:n]
+            else:
+                data[0, : len(x1)] = x1 / m1 if m1 >= 1 else x1
+            if len(x2) > n:
+                data[1, :n] = x2[:n] / m2 if m2 >= 1 else x2[:n]
+            else:
+                data[1, : len(x2)] = x2 / m2 if m2 >= 1 else x2
+        else:
+            # alignment to end
+            if len(x1) > n:
+                data[0, :n] = x1[len(x1) - n :] / m1 if m1 >= 1 else x1[len(x1) - n :]
+            else:
+                data[0, (self.buffer_length - len(x1)) :] = x1 / m1 if m1 >= 1 else x1
+
+            if len(x2) > n:
+                data[1, :n] = x2[len(x2) - n :] / m2 if m2 >= 1 else x2[len(x2) - n :]
+            else:
+                data[1, (self.buffer_length - len(x2)) :] = x2 / m2 if m2 >= 1 else x2
+
+        # use int16 for faster upload
+        interleaved_data = (data.reshape((-2,), order="F") * (2 ** 15 - 1)).astype(
+            "int16"
+        )
+        return interleaved_data
+
+    def round_up(self, n):
+        m, rest = divmod(n, self.granularity)
+        if not rest:
+            return n
+        else:
+            return (m + 1) * self.granularity
 
 
 #################################################################
