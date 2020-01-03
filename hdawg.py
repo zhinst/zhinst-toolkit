fd59426627f9f64f4aa47c8585d648a0cfea585b
Max Ruckriegel <maxr@zhinst.com>
2020-01-03 16:03:09 +0100

    add HDAWG documentation

--- hdawg.py
+++ hdawg.py
@@ -3,19 +3,17 @@ from awg import AWG
 
 
 class HDAWG(Device):
-    """HDAWG class that implements a driver for a HDAWG, inherits from ZiDevice class
-    
-    Attributes:
-    
-    Public Methods:
-        connect(self, address)
-
+    """
     Typical Usage:
-        hd = HDAWG()
-        hd.connect("dev8030")
-        hd.set_node_value("sigouts/0/on", 1)
-        hd.get_node_value("sigouts/0/on")
-
+        >>> hd = HDAWG()
+        >>> hd.connect("dev8030")
+        >>> hd.set_node_value("sigouts/0/on", 1)
+        >>> hd.get_node_value("sigouts/0/on")
+        >>> hd.setup_awg(0)
+        >>> hd.awgs[0].set(sequence_type="Simple")
+        >>> hd.awgs[0].add_waveform(np.linspace(-1, 1, 100), np.lispace(1, -1, 100))
+        >>> hd.upload_waveforms()
+        >>> hd.awgs[0].run() 
     """
 
     def __init__(self, hdawg8=True, **kwargs):
@@ -29,7 +27,7 @@ class HDAWG(Device):
         """connect to device, overwrite ZiDevice method
         
         Arguments:
-            device {string} -- device string
+            device {string} -- device ID, e.g. "dev8030"
         """
         super().connect(device, "HDAWG")
 
@@ -37,10 +35,18 @@ class HDAWG(Device):
         pass
 
     def setup_awg(self, i):
+        """setup AWG object with index i, creates awgModule object and 
+        executes it
+        
+        Arguments:
+            i {int} -- index of AWG
+        """
         self.__awgs[i].setup(self._daq, self._device)
         print("Started AWG {} of device {}".format(i, self._device))
 
     @property
     def awgs(self):
+        """use property awgs to access list of AWG objects
+        """
         return self.__awgs
 
