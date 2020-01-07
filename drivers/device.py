c316f769cb9fe2c154b6df31fbd8aa5adb488cea
Max Ruckriegel <maxr@zhinst.com>
2020-01-07 10:11:28 +0100

    refactor set() of Device class

--- drivers/device.py
+++ drivers/device.py
@@ -9,46 +9,16 @@ class DeviceTypeError(Exception):
 
 
 class Device(object):
-    """ZiDevice class that implements basic functionality common to any ZI device
-    
-    Attributes:
-        daq {ziDAQmodule} -- ziPython DAQ module used for the data connection
-        device {string} -- device string, e.g. "dev8030"
-    
-    Public Methods:
-        connect(self, address, device_type)
-        set_node_value(self, node, value)
-        get_node_value(self, node)
-
-    Typical Usage:
-        device = Device()
-        device.connect("dev8030", "HDAWG")
-        device.set_node_value("sigouts/0/on", 1)
-        device.get_node_value("sigouts/0/on")
-
-    """
-
     def connect(self, address, device_type=None, port=8004, api_level=6):
-        """open connection to HDAWG device, initialize DAQ object
-        
-        Arguments:
-            object {self} -- self
-            address {string} -- device address, e.g. "dev8030"
-            device_type {string} -- required device type, e.g. {"HDAWG", "UHFQA"}
-        
-        Keyword Arguments:
-            port {int} -- default port to discover device (default: {8004})
-            api_level {int} -- ziPython API level (default: {6})
-        """
         if address == "":
-            self._daq = zhinst.utils.autoConnect(
+            self.__daq = zhinst.utils.autoConnect(
                 default_port=port, api_level=api_level
             )
-            self._device = zhinst.utils.autoDetect(self._daq)
+            self._device = zhinst.utils.autoDetect(self.__daq)
         else:
             if device_type is None:
                 raise DeviceTypeError("Device type must be specified")
-            (self._daq, self._device, _) = zhinst.utils.create_api_session(
+            (self.__daq, self._device, _) = zhinst.utils.create_api_session(
                 address,
                 api_level,
                 required_devtype=device_type,
@@ -56,53 +26,21 @@ class Device(object):
             )
         return
 
-    def set_node(self, set_command, value):
-        """sets the node value of the given node
-        
-        Arguments:
-            set_command {string} -- node path, device does not need to be specified
-            value {} -- value to be set, various datatypes
-        
-        Returns:
-            vaious datatypes -- value actually set on device
-        """
-        node = self._command_to_node(set_command)
-        if "/zi/" in set_command.lower():
-            node = set_command
-        dtype = self.__get_node_datatype(node)
-        self.__set_parameter(node, dtype(value))
-        return self.get_node(node)
-
-    def __set_parameter(self, node, value):
-        """set value for given node depending on datatype
-        
-        Arguments:
-            node {string} -- node path, device needs to be be specified
-            value {various} -- actual value to be set
-        """
-        if isinstance(value, float):
-            self._daq.asyncSetDouble(node, value)
-        elif isinstance(value, int):
-            self._daq.asyncSetInt(node, value)
-        elif isinstance(value, str):
-            self._daq.asyncSetString(node, value)
-        elif isinstance(value, complex):
-            self._daq.setComplex(node, value)
-        return
+    def set(self, *args):
+        if len(args) == 2:
+            settings = [(args[0], args[1])]
+        elif len(args) == 1:
+            settings = args[0]
+        else:
+            raise Exception("Invalid number of arguments!")
+        settings = self.__commands_to_node(settings)
+        return self.__daq.set(settings)
 
     def get_node(self, get_command):
-        """method to get value of node from device
-        
-        Arguments:
-            node {string} -- node path, device may be specified
-        
-        Returns:
-            various -- value on device
-        """
-        node = self._command_to_node(get_command)
+        node = self.__command_to_node(get_command)
         dtype = self.__get_node_datatype(node)
         # read data from ZI
-        d = self._daq.get(node, flat=True)
+        d = self.__daq.get(node, flat=True)
         assert len(d) > 0
         # extract and return data
         data = next(iter(d.values()))
@@ -113,21 +51,13 @@ class Device(object):
         return value
 
     def __get_node_datatype(self, node):
-        """return datatype of node
-        
-        Arguments:
-            node {string} -- node path, device must be specified
-        
-        Returns:
-            dtype -- datatype of node, in {str, int, float, complex}
-        """
         # used cached value, if available
         if not hasattr(self, "_node_datatypes"):
             self.__node_datatypes = dict()
         if node in self.__node_datatypes:
             return self.__node_datatypes[node]
         # find datatype from returned data
-        d = self._daq.get(node, flat=True)
+        d = self.__daq.get(node, flat=True)
         assert len(d) > 0
         data = next(iter(d.values()))
         # if returning dict, strip timing information (API level 6)
@@ -153,7 +83,15 @@ class Device(object):
         self.__node_datatypes[node] = dtype
         return dtype
 
-    def _command_to_node(self, command):
+    
+    def __commands_to_node(self, settings):
+        new_settings = []
+        for args in settings:
+            new_settings.append( (self.__command_to_node(args[0]), args[1]) )
+        return new_settings
+    
+    
+    def __command_to_node(self, command):
         command = command.lower()
         if command[0] != "/":
             command = "/" + command
