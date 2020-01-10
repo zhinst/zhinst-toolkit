479915d1b317792d8f59e14646e2e9e1916d3333
Max Ruckriegel <maxr@zhinst.com>
2020-01-10 17:12:35 +0100

    copy Device, HDAWG and UHFQA from Hoss
    - copy constructors with everything
    - comment out connect(), setup(), ... that need daq
    - awgs in HD and QA are dictionaries now

--- drivers/device.py
+++ drivers/device.py
@@ -1,6 +1,7 @@
 import zhinst
 import zhinst.utils
 import numpy as np
+import networkx as nx
 
 
 class DeviceTypeError(Exception):
@@ -8,22 +9,79 @@ class DeviceTypeError(Exception):
 
 
 class Device(object):
-    def connect(self, address, device_type=None, port=8004, api_level=6):
-        if address == "":
-            self._daq = zhinst.utils.autoConnect(
-                default_port=port, api_level=api_level
-            )
-            self._device = zhinst.utils.autoDetect(self._daq)
-        else:
-            if device_type is None:
-                raise DeviceTypeError("Device type must be specified")
-            (self._daq, self._device, _) = zhinst.utils.create_api_session(
-                address,
-                api_level,
-                required_devtype=device_type,
-                required_err_msg=f"This driver requires a {device_type}!"
-            )
-        return
+    def __init__(self, **kwargs):
+        if "name" not in kwargs:
+            raise Exception("A device needs a name")
+        self.__name = kwargs.get("name")
+        self.__serial = self.__interface = str()
+        self.__graph = nx.DiGraph()
+        self.__component = True if kwargs.get("main_component") == "true" else False
+        self.__graph.add_node(
+            kwargs["name"], device=self, main_component=self.__component
+        )
+        self.__labels = (
+            {self.__name: self.__name}
+            if "label" not in kwargs
+            else {self.__name: kwargs["label"]}
+        )
+
+        if self.__component is True:
+            self.__serial = kwargs["properties"]["config"]["serial"]
+            self.__interface = kwargs["properties"]["config"]["interface"]
+
+    def add_node(self, **kwargs: dict) -> None:
+        self.__labels[kwargs["name"]] = (
+            kwargs["name"] if "label" not in kwargs else kwargs["label"]
+        )
+        self.add_edge(self.__name, kwargs["name"])
+
+    def add_edge(self, src, dst):
+        self.__graph.add_edge(src, dst)
+
+    @property
+    def graph(self):
+        return self.__graph
+
+    @property
+    def labels(self):
+        return self.__labels
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def serial(self):
+        return self.__serial
+
+    @property
+    def interface(self):
+        return self.__interface
+
+    def compose(self, other_graph):
+        self.__graph = nx.compose(self.__graph, other_graph)
+
+    def update_labels(self, labels):
+        self.__labels.update(labels)
+
+    # def connect(self, address, device_type=None, port=8004, api_level=6):
+    #     if address == "":
+    #         self._daq = zhinst.utils.autoConnect(
+    #             default_port=port, api_level=api_level
+    #         )
+    #         self._device = zhinst.utils.autoDetect(self._daq)
+    #     else:
+    #         if device_type is None:
+    #             raise DeviceTypeError("Device type must be specified")
+    #         (self._daq, self._device, _) = zhinst.utils.create_api_session(
+    #             address,
+    #             api_level,
+    #             required_devtype=device_type,
+    #             required_err_msg=f"This driver requires a {device_type}!"
+    #         )
+    #     return
+
+    # set and get node values here ...
 
     def set(self, *args):
         if len(args) == 2:
@@ -43,7 +101,7 @@ class Device(object):
                 paths.append(self.__command_to_node(c))
             node_string = ", ".join([p for p in paths])
         elif isinstance(command, str):
-            node_string = self.__command_to_node(command) 
+            node_string = self.__command_to_node(command)
         else:
             raise Exception("Invalid argument!")
         data = self._daq.get(node_string, settingsonly=False, flat=True)
@@ -52,10 +110,10 @@ class Device(object):
             if len(data) > 1:
                 return [v for v in data.values()]
             else:
-                return  list(data.values())[0]
+                return list(data.values())[0]
         else:
             return data
-        
+
     def __get_value_from_dict(self, data):
         if not isinstance(data, dict):
             raise Exception("Something went wrong...")
@@ -71,18 +129,18 @@ class Device(object):
             if "vector" in data_dict.keys():
                 new_data[key] = data_dict["vector"]
         return new_data
-    
+
     def __commands_to_node(self, settings):
         new_settings = []
         for args in settings:
             try:
-                if len(args) != 2: 
+                if len(args) != 2:
                     raise Exception("node/value must be specified as pairs!")
             except TypeError:
                 raise Exception("node/value must be specified as pairs!")
-            new_settings.append( (self.__command_to_node(args[0]), args[1]) )
+            new_settings.append((self.__command_to_node(args[0]), args[1]))
         return new_settings
-    
+
     def __command_to_node(self, command):
         command = command.lower()
         if command[0] != "/":
--- drivers/hdawg.py
+++ drivers/hdawg.py
@@ -3,22 +3,31 @@ from .awg import AWG
 
 
 class HDAWG(Device):
-    def __init__(self, hdawg8=True, **kwargs):
-        self.__n_channels = 8 if hdawg8 else 4
-        self.__awgs = []
-        for i in range(int(self.__n_channels / 2)):
-            self.__awgs.append(AWG(index=i))
-        pass
+    def __init__(self, **kwargs):
+        self.__awgs = {}
+        super().__init__(**kwargs, main_component="true")
 
-    def connect(self, device):
-        super().connect(device, "HDAWG")
+        for key, value in kwargs["properties"].items():
+            if key.startswith("awg"):
+                super().add_node(name=super().name + key)
+                self.__awgs[key] = AWG(
+                    name=super().name + key, properties=value, label=key
+                )
+                super().update_labels(self.__awgs[key].labels)
+                super().compose(self.__awgs[key].graph)
+            elif key.startswith("dio"):
+                if value is not None:
+                    super().add_node(name=value)
+
+    # def connect(self, device):
+    #     super().connect(device, "HDAWG")
 
     def disconnect(self):
         pass
 
-    def setup_awg(self, i):
-        self.__awgs[i].setup(self._daq, self._device)
-        print(f"Started AWG {i} of device {self._device}")
+    # def setup_awg(self, i):
+    #     self.__awgs[i].setup(self._daq, self._device)
+    #     print(f"Started AWG {i} of device {self._device}")
 
     @property
     def awgs(self):
