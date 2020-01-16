a28f50b9eef12762cb6c1743646128363c7364bc
Max Ruckriegel <maxr@zhinst.com>
2020-01-16 11:17:06 +0100

    Update example

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+from helpers import Waveform
 from controller import Controller
 from controller.drivers.connection import ZIDeviceConnection
 import numpy as np
@@ -6,21 +7,20 @@ import time
 
 if __name__ == "__main__":
 
-    class Details:
-        def __init__(self):
-            self.host = "localhost"
-            self.port = 8004
-            self.api = 6
+    c = Controller()
+    c.setup("resources/connection.json")
+    c.connect_device("hdawg0")
 
-    c = ZIDeviceConnection(Details())
-    c.connect()
+    # c.awg_set_sequence_params(0, sequence_type="Rabi")
+    # c.compile_program(0)
 
-    print(c.awgModule.get("*"), index=1)
+    c.awg_set_sequence_params(0, sequence_type="Simple")
 
-    # c = Controller()
-    # c.setup("resources/connection.json")
-    # c.connect_device("hdawg0")
+    n = 100
+    for i in range(n):
+        wave = i / n * np.ones(1000)
+        c.awg_queue_waveform(0, Waveform(wave, wave))
 
-    # c.set("sigouts/0/on", 1)
-    # time.sleep(3)
-    # c.set("sigouts/0/on", 0)
+    c.awg_upload_waveforms(0)
+
+    print("Done!")
