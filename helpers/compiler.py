f97a3eb653fb73dd26e73b26e5f6c4a9addd4bf9
Max Ruckriegel <maxr@zhinst.com>
2020-01-20 12:42:11 +0100

    Add support for multiple devices, e.g. HDAWG+UHFQA
    - Make attribute devices to dict()
    - Add add_device to Compiler
    - Change compiler to use keys: name+"-"+awg
    - Change all methods to use self._devices[name] instead of self._device
    - Add parameter name in all methods
    - Need to now specify (name, awg, ***)
    - Change awgs in QA to dict
    - Add json exmple for two devices
    - Add example for HD+QA Rabi

--- helpers/compiler.py
+++ helpers/compiler.py
@@ -2,23 +2,30 @@ from helpers import SequenceProgram
 
 
 class Compiler:
-    def __init__(self, device, **kwargs):
+    def __init__(self, **kwargs):
         self.__sequencers = dict()
-        self.__device = device
-        for i in self.__device.connectivity.awgs:
-            self.__sequencers[i.awg] = SequenceProgram()
-            self.__sequencers[i.awg].set(target=self.__device.config.device_type)
-        # maybe add channel grouping or something
+        self.__devices = list()
 
-    def set_parameter(self, awg, **kwargs):
-        self.__sequencers[awg].set(**kwargs)
+    def add_device(self, device):
+        self.__devices.append(device)
+        for i in device.connectivity.awgs:
+            self.__sequencers[self.__to_key(device.name, i.awg)] = SequenceProgram()
+            self.__sequencers[self.__to_key(device.name, i.awg)].set(
+                target=device.config.device_type
+            )
 
-    def list_params(self, awg):
-        return self.__sequencers[awg].list_params()
+    def set_parameter(self, dev, awg, **kwargs):
+        self.__sequencers[self.__to_key(dev, awg)].set(**kwargs)
 
-    def get_program(self, awg):
-        return self.__sequencers[awg].get()
+    def list_params(self, dev, awg):
+        return self.__sequencers[self.__to_key(dev, awg)].list_params()
 
-    def sequence_type(self, awg):
-        return self.__sequencers[awg].sequence_type
+    def get_program(self, dev, awg):
+        return self.__sequencers[self.__to_key(dev, awg)].get()
+
+    def sequence_type(self, dev, awg):
+        return self.__sequencers[self.__to_key(dev, awg)].sequence_type
+
+    def __to_key(self, dev, awg):
+        return str(dev + "-" + str(awg))
 
