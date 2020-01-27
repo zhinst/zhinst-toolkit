40826eaeaacd4aa7f2638d59f15ceec62a083d28
Max Ruckriegel <maxr@zhinst.com>
2020-01-27 10:28:02 +0100

    Add property program to AWG

--- ziDrivers/devices/awg.py
+++ ziDrivers/devices/awg.py
@@ -17,16 +17,12 @@ class AWG:
             AWGChannel(i.wave, i.trig, i.marker)
             for i in (configuration.ch0, configuration.ch1)
         ]
-        # super().__init__(name=self.id, parent=None)
         self.parent = parent
         self.__waveforms = list()
         self.__program = str()
 
     def set_program(self, program):
         self.__program = program
-
-    def get_program(self):
-        return self.__program
 
     def reset_waveforms(self):
         self.__waveforms = list()
@@ -35,20 +31,7 @@ class AWG:
     def waveforms(self):
         return self.__waveforms
 
-        # for i in range(len(self._channels)):
-        #     super().add_edge(
-        #         src=hash(self),
-        #         dst=str(hash(self)) + str(i),
-        #         src_label=self.id,
-        #         dst_label=str(i),
-        #         edge_label=None,
-        #     )
+    @property
+    def program(self):
+        return None if self.__program == "" else self.__program
 
-        #     if self._channels[i].wave is not None:
-        #         super().add_edge(
-        #             src=str(hash(self)) + str(i),
-        #             dst=self._channels[i].wave,
-        #             src_label=str(i),
-        #             dst_label=self._channels[i].wave,
-        #             edge_label=None,
-        #         )
