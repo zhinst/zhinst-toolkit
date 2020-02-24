eaba32d0a9e552b284c080ddfa9bea38d2262782
Max Ruckriegel <maxr@zhinst.com>
2020-02-24 10:22:53 +0100

    Add first version of MultiDevice that adds multiple instruments to same connection

--- ziDrivers/__init__.py
+++ ziDrivers/__init__.py
@@ -1,1 +1,3 @@
-from .controller import Controller
+from .HDAWGController import HDAWGController
+from .UHFQAController import UHFQAController
+from .MultiDeviceController import MultiDeviceController
