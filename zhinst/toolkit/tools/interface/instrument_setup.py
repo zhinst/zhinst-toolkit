2aeabacfdc14c2ed89d873451e0c18988193c568
Max Ruckriegel <maxr@zhinst.com>
2020-02-26 18:48:45 +0100

    Refactor interface to not use JSON/deserialization

--- zhinst/toolkit/tools/interface/instrument_setup.py
+++ zhinst/toolkit/tools/interface/instrument_setup.py
@@ -1,174 +1,41 @@
-from dataclasses import dataclass
-
-from marshmallow import Schema, fields, post_load, EXCLUDE
+import attr
 
 
-class ZIDIOConfig(Schema):
-    device = fields.Str()
-
-    @dataclass
-    class Data:
-        device: str
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
+@attr.s
+class ZIDeviceConfig:
+    serial: str = attr.ib(default="dev#####")
+    device_type: str = attr.ib(
+        default="hdawg", validator=attr.validators.in_(["hdawg", "uhfqa", "pqsc"])
+    )
+    interface: str = attr.ib(default="1GbE")
 
 
-class ZIAWGChannelConfig(Schema):
-    wave = fields.Str(allow_none=True)
-    trig = fields.Str(allow_none=True)
-    marker = fields.Str(allow_none=True)
+class ZIDevice:
+    def __init__(self):
+        self._config = ZIDeviceConfig()
+        self.name = "device0"
 
-    @dataclass
-    class Data:
-        wave: str = None
-        trig: str = None
-        marker: str = None
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
+    @property
+    def config(self):
+        return self._config
 
 
-class ZIAWGConfig(Schema):
-    iq = fields.Boolean()
-
-    @dataclass
-    class Data:
-        iq: bool
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
+@attr.s
+class ZIAPI:
+    host: str = attr.ib(default="localhost")
+    port: int = attr.ib(default=8004)
+    api: int = attr.ib(default=6)
 
 
-class ZIAWG(Schema):
-    awg = fields.Integer()
-    config = fields.Nested(ZIAWGConfig)
-    ch0 = fields.Nested(ZIAWGChannelConfig)
-    ch1 = fields.Nested(ZIAWGChannelConfig)
+class InstrumentConfiguration:
+    def __init__(self):
+        self._api_config = ZIAPI()
+        self._instrument = ZIDevice()
 
-    @dataclass
-    class Data:
-        awg: int
-        config: ZIAWGConfig.Data
-        ch0: ZIAWGChannelConfig.Data
-        ch1: ZIAWGChannelConfig.Data
+    @property
+    def api_config(self):
+        return self._api_config
 
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
-
-
-class ZIDEVICEIO(Schema):
-    awgs = fields.List(fields.Nested(ZIAWG))
-    dio = fields.Nested(ZIDIOConfig)
-
-    @dataclass
-    class Data:
-        awgs: ZIAWG.Data
-        dio: ZIDIOConfig.Data
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
-
-
-class ZIDeviceConfig(Schema):
-    serial = fields.Str()
-    device_type = fields.Str()
-    interface = fields.Str()
-
-    @dataclass
-    class Data:
-        serial: str
-        device_type: str
-        interface: str
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
-
-
-class ZIDevice(Schema):
-    name = fields.Str()
-    config = fields.Nested(ZIDeviceConfig)
-    connectivity = fields.Nested(ZIDEVICEIO)
-
-    @dataclass
-    class Data:
-        name: str
-        config: ZIDeviceConfig.Data
-        connectivity: ZIDEVICEIO.Data
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
-
-
-class Instrument(Schema):
-    provider = fields.Str()
-    setup = fields.List(fields.Nested(ZIDevice))
-
-    @dataclass
-    class Data:
-        provider: str
-        setup: ZIDevice.Data
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
-
-
-class ZIAPI(Schema):
-    host = fields.Str()
-    port = fields.Integer()
-    api = fields.Integer()
-
-    class Meta:
-        fields = ("host", "port", "api")
-        unknown = EXCLUDE
-
-    @dataclass
-    class Data:
-        host: str
-        port: int
-        api: int
-        name: str = None
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
-
-
-class API(Schema):
-    provider = fields.Str()
-    details = fields.Nested(ZIAPI)
-
-    @dataclass
-    class Data:
-        provider: str
-        details: ZIAPI.Data
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
-
-
-class InstrumentConfiguration(Schema):
-    class Meta:
-        fields = ("api_configs", "instruments")
-        unknown = EXCLUDE
-
-    @dataclass
-    class Data:
-        api_configs: API.Data
-        instruments: Instrument.Data
-
-    api_configs = fields.List(fields.Nested(API))
-    instruments = fields.List(fields.Nested(Instrument))
-
-    @post_load
-    def make_(self, data, **kwargs):
-        return self.Data(**data)
+    @property
+    def instrument(self):
+        return self._instrument
