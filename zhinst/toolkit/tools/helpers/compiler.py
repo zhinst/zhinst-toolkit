c557c00e49824f91d656f756f810f499ed43c403
Max Ruckriegel <maxr@zhinst.com>
2020-02-26 18:50:55 +0100

    Adapt to use new interface definition

--- zhinst/toolkit/tools/helpers/compiler.py
+++ zhinst/toolkit/tools/helpers/compiler.py
@@ -3,28 +3,28 @@ from .sequenceProgram import SequenceProgram
 
 class Compiler:
     def __init__(self, **kwargs):
-        self.__sequencers = dict()
-        self.__devices = list()
+        self._sequencers = dict()
+        self._devices = list()
 
     def add_device(self, device):
-        self.__devices.append(device)
-        for i in device.connectivity.awgs:
-            self.__sequencers[self.__to_key(device.name, i.awg)] = SequenceProgram()
-            self.__sequencers[self.__to_key(device.name, i.awg)].set(
-                target=device.config.device_type
+        self._devices.append(device)
+        for i in device.awgs.keys():
+            self._sequencers[self._to_key(device.name, i)] = SequenceProgram()
+            self._sequencers[self._to_key(device.name, i)].set(
+                target=device.device_type
             )
 
     def set_parameter(self, dev, awg, **kwargs):
-        self.__sequencers[self.__to_key(dev, awg)].set(**kwargs)
+        self._sequencers[self._to_key(dev, awg)].set(**kwargs)
 
     def list_params(self, dev, awg):
-        return self.__sequencers[self.__to_key(dev, awg)].list_params()
+        return self._sequencers[self._to_key(dev, awg)].list_params()
 
     def get_program(self, dev, awg):
-        return self.__sequencers[self.__to_key(dev, awg)].get()
+        return self._sequencers[self._to_key(dev, awg)].get()
 
     def sequence_type(self, dev, awg):
-        return self.__sequencers[self.__to_key(dev, awg)].sequence_type
+        return self._sequencers[self._to_key(dev, awg)].sequence_type
 
-    def __to_key(self, dev, awg):
+    def _to_key(self, dev, awg):
         return str(dev + "-" + str(awg))
