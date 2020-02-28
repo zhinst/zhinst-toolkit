3f03f0619a253180ad04ecd2c2d5faba899a0c68
Max Ruckriegel <maxr@zhinst.com>
2020-02-28 09:58:32 +0100

    Refactor compiler to support only one device
    - Turn devices list into device
    - Remove name everywhere

--- zhinst/toolkit/tools/helpers/compiler.py
+++ zhinst/toolkit/tools/helpers/compiler.py
@@ -4,27 +4,25 @@ from .sequenceProgram import SequenceProgram
 class Compiler:
     def __init__(self, **kwargs):
         self._sequencers = dict()
-        self._devices = list()
+        self._device = None
 
     def add_device(self, device):
-        self._devices.append(device)
+        self._device = device
         for i in device.awgs.keys():
-            self._sequencers[self._to_key(device.name, i)] = SequenceProgram()
-            self._sequencers[self._to_key(device.name, i)].set(
-                target=device.device_type
-            )
+            self._sequencers[self._to_key(i)] = SequenceProgram()
+            self._sequencers[self._to_key(i)].set(target=device.device_type)
 
-    def set_parameter(self, dev, awg, **kwargs):
-        self._sequencers[self._to_key(dev, awg)].set(**kwargs)
+    def set_parameter(self, awg, **kwargs):
+        self._sequencers[self._to_key(awg)].set(**kwargs)
 
-    def list_params(self, dev, awg):
-        return self._sequencers[self._to_key(dev, awg)].list_params()
+    def list_params(self, awg):
+        return self._sequencers[self._to_key(awg)].list_params()
 
-    def get_program(self, dev, awg):
-        return self._sequencers[self._to_key(dev, awg)].get()
+    def get_program(self, awg):
+        return self._sequencers[self._to_key(awg)].get()
 
-    def sequence_type(self, dev, awg):
-        return self._sequencers[self._to_key(dev, awg)].sequence_type
+    def sequence_type(self, awg):
+        return self._sequencers[self._to_key(awg)].sequence_type
 
-    def _to_key(self, dev, awg):
-        return str(dev + "-" + str(awg))
+    def _to_key(self, awg):
+        return str(self._device.name + "-" + str(awg))
