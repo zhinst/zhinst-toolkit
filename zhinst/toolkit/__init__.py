03a76e861d5b22ffa16181a6af77726890bffe0b
Max Ruckriegel <maxr@zhinst.com>
2020-03-03 15:04:05 +0100

    Add MFLI instrument

--- zhinst/toolkit/__init__.py
+++ zhinst/toolkit/__init__.py
@@ -2,5 +2,6 @@ from .base import BaseInstrument
 from .hdawg import HDAWG
 from .uhfqa import UHFQA
 from .uhfli import UHFLI
+from .mfli import MFLI
 from .pqsc import PQSC
 from .multi_device_connection import MultiDeviceConnection
