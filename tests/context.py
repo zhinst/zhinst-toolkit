32362e766861113542569ac08b87c596f5ca085c
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-03-16 14:45:39 +0100

    Update context for tests to new imports

---
+++ tests/context.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020 Zurich Instruments
+#
+# This software may be modified and distributed under the terms
+# of the MIT license. See the LICENSE file for details.
+
+
+import sys
+
+
+from zhinst.toolkit import HDAWG, UHFQA, UHFLI, MFLI, PQSC, MultiDeviceConnection
+from zhinst.toolkit.control.drivers.base import BaseInstrument, AWGCore
+from zhinst.toolkit.control.nodetree import Nodetree, Parameter, NodeList, Node
+from zhinst.toolkit.helpers import Waveform
+from zhinst.toolkit.helpers import SeqCommand
+from zhinst.toolkit.helpers import SequenceProgram
+from zhinst.toolkit.interface import InstrumentConfiguration
+
