61c2a2015d49be60ccd22f552a77d8b69ca78ab4
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-03-18 19:09:02 +0100

    Move module connections back and rename

--- zhinst/toolkit/control/connection.py
+++ zhinst/toolkit/control/connection.py
@@ -5,7 +5,6 @@
 
 import json
 import zhinst.ziPython as zi
-from control.modules import AWGModule, DAQModule
 from zhinst.toolkit.interface import DeviceTypes
 
 
@@ -68,8 +67,8 @@ class ZIConnection:
                 f"{self._connection_details.port} "
                 f"api version: {self._connection_details.api}"
             )
-            self._awg = AWGModule(self._daq)
-            self._daq_module = DAQModule(self._daq)
+            self._awg_module = AWGModuleConnection(self._daq)
+            self._daq_module = DAQModuleConnection(self._daq)
         else:
             raise ZHTKConnectionException(
                 f"No connection could be established with the connection details:"
@@ -100,7 +99,7 @@ class ZIConnection:
         print(
             f"Successfully connected to device {serial.upper()} on interface {interface.upper()}"
         )
-        self._awg.update(device=serial, index=0)
+        self._awg_module.update(device=serial, index=0)
 
     def set(self, *args):
         """
@@ -236,11 +235,161 @@ class ZIConnection:
 
     @property
     def awg_module(self):
-        return self._awg
+        return self._awg_module
 
     @property
     def daq_module(self):
         return self._daq_module
+
+
+class AWGModuleConnection:
+    """
+    Implements an awg module as daq.awgModule(...) in zhinst.ziPython with 
+    get and set methods of the module. Allows to address different awgs on 
+    different devices with the same awg module using the update(...) method. 
+    """
+
+    def __init__(self, daq):
+        self._awgModule = daq.awgModule()
+        self._awgModule.execute()
+        self._device = self._awgModule.getString("/device")
+        self._index = self._awgModule.getInt("/index")
+
+    def set(self, *args, **kwargs):
+        self.update(**kwargs)
+        self._awgModule.set(*args)
+
+    def get(self, *args, **kwargs):
+        self.update(**kwargs)
+        return self._awgModule.get(*args, flat=True)
+
+    def get_int(self, *args, **kwargs):
+        self.update(**kwargs)
+        return self._awgModule.getInt(*args)
+
+    def get_double(self, *args, **kwargs):
+        self.update(**kwargs)
+        return self._awgModule.getDouble(*args)
+
+    def get_string(self, *args, **kwargs):
+        self.update(**kwargs)
+        return self._awgModule.getString(*args)
+
+    def update(self, **kwargs):
+        if "device" in kwargs.keys():
+            self._update_device(kwargs["device"])
+        if "index" in kwargs.keys():
+            self._update_index(kwargs["index"])
+
+    def _update_device(self, device):
+        if device != self.device:
+            self._update_index(0)
+            self._awgModule.set("/device", device)
+            self._device = device
+
+    def _update_index(self, index):
+        if index != self.index:
+            self._awgModule.set("/index", index)
+            self._index = index
+
+    @property
+    def index(self):
+        return self._index
+
+    @property
+    def device(self):
+        return self._device
+
+
+class DAQModuleConnection:
+    def __init__(self, daq):
+        self._module = daq.dataAcquisitionModule()
+        self._device = self._module.getString("/device")
+
+    def execute(self, device=None):
+        if device is not None:
+            self.update_device(device)
+        self._module.execute()
+
+    def finish(self, device=None):
+        if device is not None:
+            self.update_device(device)
+        self._module.finish()
+
+    def progress(self, device=None):
+        if device is not None:
+            self.update_device(device)
+        return self._module.progress()
+
+    def trigger(self, device=None):
+        if device is not None:
+            self.update_device(device)
+        self._module.trigger()
+
+    def read(self, device=None):
+        if device is not None:
+            self.update_device(device)
+        return self._module.read()
+
+    def subscribe(self, *args, device=None):
+        if device is not None:
+            self.update_device(device)
+        self._module.subscribe(*args)
+
+    def unsubscribe(self, *args, device=None):
+        if device is not None:
+            self.update_device(device)
+        self._module.unsubscribe(*args)
+
+    def save(self, *args, device=None):
+        if device is not None:
+            self.update_device(device)
+        self._module.save(*args)
+
+    def clear(self, device=None):
+        if device is not None:
+            self.update_device(device)
+        self._module.clear()
+
+    def set(self, *args, device=None):
+        if device is not None:
+            self.update_device(device)
+        self._module.set(*args)
+
+    def get(self, *args, device=None):
+        if device is not None:
+            self.update_device(device)
+        return self._module.get(*args, flat=True)
+
+    def get_int(self, *args, device=None):
+        if device is not None:
+            self.update_device(device)
+        return self._module.getInt(*args)
+
+    def get_double(self, *args, device=None):
+        if device is not None:
+            self.update_device(device)
+        return self._module.getDouble(*args)
+
+    def get_string(self, *args, device=None):
+        if device is not None:
+            self.update_device(device)
+        return self._module.getString(*args)
+
+    def get_nodetree(self, prefix, device=None, **kwargs):
+        if device is not None:
+            self.update_device(device)
+        tree = json.loads(self._module.listNodesJSON(prefix, **kwargs))
+        return tree
+
+    def update_device(self, device):
+        if device != self.device:
+            self._module.set("/device", device)
+            self._device = device
+
+    @property
+    def device(self):
+        return self._device
 
 
 class DeviceConnection(object):
