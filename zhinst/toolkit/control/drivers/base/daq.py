9ab9ae67f19350e19761d8be1afb1c90778953da
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-03-19 15:18:32 +0100

    Add all nodes to DAQ module

--- zhinst/toolkit/control/drivers/base/daq.py
+++ zhinst/toolkit/control/drivers/base/daq.py
@@ -1,4 +1,5 @@
 from .base import ZHTKException
+from control.nodetree import Parameter
 
 
 class DAQModule:
@@ -8,17 +9,32 @@ class DAQModule:
 
     def _setup(self):
         self._module = self._parent._controller._connection.daq_module
+        # add all parameters from nodetree
+        nodetree = self._module.get_nodetree("*")
+        for k, v in nodetree.items():
+            name = k[1:].replace("/", "_")
+            setattr(self, name, Parameter(self, v, device=self))
+        self._init_settings()
 
     def _set(self, *args):
         if self._module is None:
             raise ZHTKException("This DAQ is not connected to a dataAcquisitionModule!")
-        self._module.set(*args, device=self._parent.serial)
+        return self._module.set(*args, device=self._parent.serial)
 
     def _get(self, *args, valueonly=True):
         if self._module is None:
             raise ZHTKException("This DAQ is not connected to a dataAcquisitionModule!")
         data = self._module.get(*args, device=self._parent.serial)
         return list(data.values())[0][0] if valueonly else data
+
+    def _init_settings(self):
+        self._set("preview", 1)
+        self._set("historylength", 10)
+        self._set("bandwidth", 0)
+        self._set("hysteresis", 0.01)
+        self._set("level", 0.1)
+        self._set("clearhistory", 1)
+        self._set("bandwidth", 0)
 
     # here have some higher level 'measure()' mthod?? that combines subscribe read unsubscribe
 
