5b532bd98132808727dc5f40a6ab788a6a6840d3
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-03-20 17:57:00 +0100

    Add signals and measure() to daq module
    - signals specified by keyword, will maybe change
    - add a signal by signals_add
    - measure by running routine
    - results are in daq.results, for now

--- zhinst/toolkit/control/drivers/base/daq.py
+++ zhinst/toolkit/control/drivers/base/daq.py
@@ -1,3 +1,5 @@
+import time
+
 from .base import ZHTKException
 from control.nodetree import Parameter
 
@@ -20,14 +22,41 @@ MAPPINGS = {
     "save_fileformat": {0: "matlab", 1: "csv", 2: "zview", 3: "sxm", 4: "hdf5",},
     "type": {
         0: "continuous",
-        1: "",
-        2: "zview",
-        3: "sxm",
-        4: "hdf5",
-        5: "csv",
-        6: "zview",
-        7: "sxm",
-        8: "hdf5",
+        1: "edge",
+        2: "dio",
+        3: "pulse",
+        4: "tracking",
+        5: "change",
+        6: "hardware",
+        7: "tracking pulse",
+        8: "eventcount",
+    },
+    "signals": {
+        "demod 1 r": "/demods/0/sample.R",
+        "demod 1 x": "/demods/0/sample.X",
+        "demod 1 y": "/demods/0/sample.Y",
+        "demod 1 theta": "/demods/0/sample.Theta",
+        "frequency 1": "/demods/0/sample.Frequency",
+        "demod 1 aux input 1": "/demods/0/sample.AuxIn0",
+        "demod 1 aux input 2": "/demods/0/sample.AuxIn1",
+        "dio 1": "/demods/0/sample.Dio",
+        "demod 2 r": "/demods/1/sample.R",
+        "demod 2 x": "/demods/1/sample.X",
+        "demod 2 y": "/demods/1/sample.Y",
+        "demod 2 theta": "/demods/1/sample.Theta",
+        "frequency 2": "/demods/1/sample.Frequency",
+        "demod 2 aux input 1": "/demods/1/sample.AuxIn0",
+        "demod 2 aux input 2": "/demods/1/sample.AuxIn1",
+        "dio 2": "/demods/1/sample.Dio",
+        "imp real": "/imps/0/sample.RealZ",
+        "imp imag": "/imps/0/sample.ImagZ",
+        "imp abs": "/imps/0/sample.AbsZ",
+        "imp theta": "/imps/0/sample.PhaseZ",
+        "imp frequency": "/imps/0/sample.Frequency",
+        "imp param 1": "/imps/0/sample.Param0",
+        "imp param 2": "/imps/0/sample.Param1",
+        "imp drive": "/imps/0/sample.Drive",
+        "imp bias": "/imps/0/sample.Bias",
     },
 }
 
@@ -36,6 +65,8 @@ class DAQModule:
     def __init__(self, parent):
         self._parent = parent
         self._module = None
+        self._signals = {}
+        self._results = {}
 
     def _setup(self):
         self._module = self._parent._controller._connection.daq_module
@@ -66,6 +97,73 @@ class DAQModule:
         self._set("level", 0.1)
         self._set("clearhistory", 1)
         self._set("bandwidth", 0)
+
+    def signals_add(self, signal, operation="avg"):
+        if operation not in ["replace", "avg", "std"]:
+            raise Exception()
+        if operation == "replace":
+            operation = ""
+        mapping = MAPPINGS["signals"]
+        if signal not in mapping.keys():
+            raise ZHTKException(
+                f"Unknown signal. Avalable signals are {list(mapping.keys())}"
+            )
+        signal_node = "/"
+        signal_node += self._parent.serial
+        signal_node += mapping[signal]
+        signal_node += f".{operation}"
+        signal_name = f"{signal} {operation}"
+        if signal_node not in self.signals.values():
+            self._signals[signal_name] = signal_node
+
+    def signals_clear(self):
+        self._signals = {}
+
+    def signals_list(self):
+        print("available signals:")
+        for signal in MAPPINGS["signals"]:
+            print(f" - '{signal}'")
+
+    def measure(self, single=True, verbose=True, timeout=20):
+        self._set("endless", int(not single))
+        self._set("clearhistory", 1)
+        for path in self.signals.values():
+            self._module.subscribe(path)
+            if verbose:
+                print(f"subscribed to: {path}")
+        self._module.execute()
+        while not self._module.finished():
+            if verbose:
+                print(f"Progress: {(self._module.progress()[0] * 100):.1f}%")
+            tik = time.time()
+            time.sleep(0.5)
+            tok = time.time()
+            if tok - tik > timeout:
+                raise TimeoutError()
+        result = self._module.read(flat=True)
+        self._module.finish()
+        self._module.unsubscribe("*")
+        self._get_result_from_dict(result)
+
+    @property
+    def signals(self):
+        return self._signals
+
+    @property
+    def results(self):
+        return self._results
+
+    def _get_result_from_dict(self, result):
+        self._results = {}
+        for name, path in self.signals.items():
+            path = path.lower()
+            if path not in result.keys():
+                raise ZHTKException()
+            self._results[name] = {
+                "header": result[path][0]["header"],
+                "timestamp": result[path][0]["timestamp"],
+                "value": result[path][0]["value"],
+            }
 
     # here have some higher level 'measure()' mthod?? that combines subscribe read unsubscribe
 
