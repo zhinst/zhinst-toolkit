5e66ac27b9f42cd917e22afb667782dd2e6096a9
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-03-25 16:43:29 +0100

    Change import in __init__.py

--- zhinst/toolkit/__init__.py
+++ zhinst/toolkit/__init__.py
@@ -1,3 +1,2 @@
-from control.drivers import HDAWG, UHFQA, UHFLI, MFLI, PQSC
-from control.multi_device_connection import MultiDeviceConnection
-
+from zhinst.toolkit.control.drivers import HDAWG, UHFQA, UHFLI, MFLI, PQSC
+from zhinst.toolkit.control.multi_device_connection import MultiDeviceConnection
