6d6a3b95b9c084e0002877271ed824a8c64197fa
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-03-27 11:44:54 +0100

    Remove outdated test

--- tests/test_uhfli.py
+++ tests/test_uhfli.py
@@ -9,8 +9,3 @@ from .context import UHFLI, DeviceTypes
 def test_init_uhfqa():
     li = UHFLI("name", "dev1234")
     assert li.device_type == DeviceTypes.UHFLI
-    assert li._awg is not None
-    with pytest.raises(Exception):
-        li._awg_connection
-    with pytest.raises(Exception):
-        li._init_settings()
