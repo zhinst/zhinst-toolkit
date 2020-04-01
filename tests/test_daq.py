262298ec7209bef97f1468af76fb1aa88f020516
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-04-01 16:39:55 +0200

    Remove daq tests for now

--- tests/test_daq.py
+++ tests/test_daq.py
@@ -4,18 +4,3 @@ from hypothesis.stateful import rule, precondition, RuleBasedStateMachine
 import numpy as np
 
 from .context import DAQModule, ZHTKException
-
-
-def test_init_daq():
-    daq = DAQModule("parent")
-    assert daq._parent == "parent"
-    assert daq._module is None
-
-
-def test_no_connection():
-    daq = DAQModule("parent")
-    with pytest.raises(ZHTKException):
-        daq._get("device")
-    with pytest.raises(ZHTKException):
-        daq._set("device", "dev1234")
-
