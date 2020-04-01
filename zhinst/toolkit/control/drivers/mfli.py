7f6b811c5598b5afe4ba3810cf8c2973e6525e33
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-04-01 13:23:20 +0200

    Refactor sweeper module

--- zhinst/toolkit/control/drivers/mfli.py
+++ zhinst/toolkit/control/drivers/mfli.py
@@ -44,21 +44,6 @@ class MFLI(BaseInstrument):
         return self._sweeper_module
 
 
-MAPPINGS = {
-    "sweep_parameters": {
-        "auxout1offset": "auxouts/0/offset",
-        "auxout2offset": "auxouts/1/offset",
-        "auxout3offset": "auxouts/2/offset",
-        "auxout4offset": "auxouts/3/offset",
-        "demdod1phase": "demods/0/phaseshift",
-        "demdod2phase": "demods/1/phaseshift",
-        "frequency": "oscs/0/freq",
-        "output1amp": "sigouts/0/amplitudes/1",
-        "output1offset": "sigouts/0/offset",
-    },
-}
-
-
 class DAQModule(DAQ):
     def __init__(self, parent):
         super().__init__(parent, clk_rate=1.8e9)
@@ -88,25 +73,16 @@ class DAQModule(DAQ):
 
 
 class SweeperModule(Sweeper):
-    def signals_list(self):
-        return list(self._parent._streaming_nodes.keys())
-
-    def sweep_parameter_list(self):
-        return list(MAPPINGS["sweep_parameters"].keys())
-
-    def _parse_signals(self, signal_source):
-        sources = MAPPINGS["signal_sources"]
-        if signal_source.lower() not in sources.keys():
-            raise ZHTKException(f"Signal source must be in {list(sources.keys())}")
-        signal_node = "/"
-        signal_node += self._parent.serial
-        signal_node += f"{sources[signal_source]}"
-        return signal_node.lower()
-
-    def _parse_sweep_param(self, param):
-        mapping = MAPPINGS["sweep_parameters"]
-        if param not in mapping.keys():
-            raise ZHTKException(
-                f"The parameter {param} must be one of {list(mapping.keys())}"
-            )
-        return mapping[param]
+    def __init__(self, parent):
+        super().__init__(parent, clk_rate=60e6)
+        self._sweep_params = {
+            "auxout1offset": "auxouts/0/offset",
+            "auxout2offset": "auxouts/1/offset",
+            "auxout3offset": "auxouts/2/offset",
+            "auxout4offset": "auxouts/3/offset",
+            "demdod1phase": "demods/0/phaseshift",
+            "demdod2phase": "demods/1/phaseshift",
+            "frequency": "oscs/0/freq",
+            "output1amp": "sigouts/0/amplitudes/1",
+            "output1offset": "sigouts/0/offset",
+        }
