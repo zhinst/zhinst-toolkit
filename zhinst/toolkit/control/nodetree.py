2e125989a855b737018819a84d5ee49c5b3683a8
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-04-06 17:02:37 +0200

    Return the set value, no get command

--- zhinst/toolkit/control/nodetree.py
+++ zhinst/toolkit/control/nodetree.py
@@ -97,7 +97,7 @@ class Parameter:
                     value = inv_map[value]
                 value = self._set_parser(value)
                 self._device._set(self._path, value)
-                self._cached_value = self._getter()
+                self._cached_value = self._get_parser(value)
             return self._cached_value
         else:
             raise ZHTKNodetreeException("This parameter is not settable!")
