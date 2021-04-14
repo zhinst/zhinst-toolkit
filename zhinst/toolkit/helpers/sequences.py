a17c5be145873962b9c56fa7e8ee6183f9ad24e2
Bahadir Dönmez <bhdr.donmez@gmail.com>
2021-04-14 13:34:52 +0200

    Update clock rate in parent class `Sequence`
    Clock rate can be updated in `update_params` method of parent class
    `Sequence`. It makes more sense to update the clock rate in the parent
    class because clock rate only depends on the device type and it is common
    to multiple children classes for different sequence types.

--- zhinst/toolkit/helpers/sequences.py
+++ zhinst/toolkit/helpers/sequences.py
@@ -106,6 +106,12 @@ class Sequence(object):
 
     def update_params(self):
         """Update interrelated parameters."""
+        # Set the correct clock rate depending on the device type
+        if self.target in [DeviceTypes.HDAWG]:
+            self.clock_rate = 2.4e9
+        elif self.target in [DeviceTypes.UHFLI, DeviceTypes.UHFQA]:
+            self.clock_rate = 1.8e9
+        # Set the trigger commands depending on the trigger mode
         if self.trigger_mode == TriggerMode.NONE:
             self.trigger_cmd_1 = SequenceCommand.comment_line()
             self.trigger_cmd_2 = SequenceCommand.comment_line()
@@ -251,8 +257,6 @@ class SimpleSequence(Sequence):
         if len(self.buffer_lengths) > len(self.delay_times):
             n = len(self.buffer_lengths) - len(self.delay_times)
             self.delay_times = np.append(self.delay_times, np.zeros(n))
-        if self.target in [DeviceTypes.UHFQA, DeviceTypes.UHFLI]:
-            self.clock_rate = 1.8e9
 
     def check_attributes(self):
         super().check_attributes()
@@ -624,8 +628,6 @@ class ReadoutSequence(Sequence):
             self.wait_cycles = self.time_to_cycles(
                 temp - self.latency + self.trigger_delay
             )
-        if self.target in [DeviceTypes.UHFQA, DeviceTypes.UHFLI]:
-            self.clock_rate = 1.8e9
         len_f = len(self.readout_frequencies)
         len_a = len(self.readout_amplitudes)
         len_p = len(self.phase_shifts)
@@ -704,8 +706,6 @@ class PulsedSpectroscopySequence(Sequence):
             self.wait_cycles = self.time_to_cycles(
                 temp - self.latency + self.trigger_delay
             )
-        if self.target in [DeviceTypes.UHFQA, DeviceTypes.UHFLI]:
-            self.clock_rate = 1.8e9
 
 
 @attr.s
