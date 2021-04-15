53cc6b6024d0d0efedc10685424421d32d95169f
Bahadir Dönmez <bhdr.donmez@gmail.com>
2021-04-15 13:11:50 +0200

    Enable `__version__` attribute to toolkit
    Edit setup.py so that the version info is written to a file under
    `src/zhinst/toolkit/_version.py`
    
    Import the version number inside
    `zhinst/toolkit/__init__.py`

--- src/zhinst/toolkit/__init__.py
+++ src/zhinst/toolkit/__init__.py
@@ -1,3 +1,4 @@
 from zhinst.toolkit.control.drivers import HDAWG, UHFQA, UHFLI, MFLI, PQSC
 from zhinst.toolkit.control.multi_device_connection import MultiDeviceConnection
 from zhinst.toolkit.helpers.utils import SequenceType, TriggerMode, Alignment
+from zhinst.toolkit._version import version as __version__
