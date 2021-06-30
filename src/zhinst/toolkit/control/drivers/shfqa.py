77d6bf8fe1dd8ef486331578aa6f4c6416801a0b
Bahadir Dönmez <bhdr.donmez@gmail.com>
2021-06-30 15:59:54 +0200

    Add `allowed_sequences`, `allowed_trigger_modes`
    #### **The following changes are made in this update:**
    ##### **1) New device attribute `allowed_sequences`:**
    A list of allowed sequence types are added as attributes of the 
    following devices:
    * UHFQA
    * HDAWG
    * SHFQA
    
    ##### **2) New device attribute `allowed_trigger_modes`:**
    A list of allowed trigger modes are added as attributes of the 
    following devices:
    * UHFQA
    * HDAWG
    * SHFQA
    
    ##### **3) Updated list of allowed trigger modes in UHFQA `:**
    "Send Trigger" and "Send and Receive Trigger" modes are not tested on 
    UHFQA, therefore they are removed from the list of allowed trigger 
    modes.
    
    ##### **4) Updated tests to check new attributes `:**
    * `test_init_hdawg` in `test_hdawg.py`
    * `test_init_uhfqa` in `test_uhfqa.py`

--- src/zhinst/toolkit/control/drivers/shfqa.py
+++ src/zhinst/toolkit/control/drivers/shfqa.py
@@ -32,6 +32,12 @@ class SHFQA(BaseInstrument):
     four :class:`Generator` s that are specific for the device and
     inherit from the :class:`SHFGenerator` class.
 
+    Attributes:
+        allowed_sequences (list): A list of :class:`SequenceType` s
+            that the instrument supports.
+        allowed_trigger_modes (list): A list of :class:`TriggerMode` s
+            that the instrument supports.
+
     """
 
     def __init__(self, name: str, serial: str, discovery=None, **kwargs) -> None:
@@ -42,6 +48,13 @@ class SHFQA(BaseInstrument):
         self.ref_clock_actual = None
         self.ref_clock_status = None
         self.timebase = None
+        self._allowed_sequences = [
+            SequenceType.NONE,
+            SequenceType.CUSTOM,
+        ]
+        self._allowed_trigger_modes = [
+            TriggerMode.NONE,
+        ]
 
     def connect_device(self, nodetree: bool = True) -> None:
         """Connects the device to the data server.
@@ -132,6 +145,14 @@ class SHFQA(BaseInstrument):
     @property
     def scope(self):
         return self._scope
+
+    @property
+    def allowed_sequences(self):
+        return self._allowed_sequences
+
+    @property
+    def allowed_trigger_modes(self):
+        return self._allowed_trigger_modes
 
 
 class Channel(SHFChannel):
@@ -297,6 +318,22 @@ class Generator(SHFGenerator):
             ),
             device=self._device,
         )
+    
+    def _apply_sequence_settings(self, **kwargs):
+        # check sequence type
+        if "sequence_type" in kwargs.keys():
+            t = SequenceType(kwargs["sequence_type"])
+            if t not in self._device.allowed_sequences:
+                raise ToolkitError(
+                    f"Sequence type {t} must be one of {[s.value for s in self._device.allowed_sequences]}!"
+                )
+        # check trigger mode
+        if "trigger_mode" in kwargs.keys():
+            t = TriggerMode(kwargs["trigger_mode"])
+            if t not in self._device.allowed_trigger_modes:
+                raise ToolkitError(
+                    f"Trigger mode {t} must be one of {[s.value for s in self._device.allowed_trigger_modes]}!"
+                )
 
 
 class Sweeper(SHFSweeper):
