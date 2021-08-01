eeec1c229b1c1ab379b8cef302e862a975a6b5ee
Bahadir Dönmez <bhdr.donmez@gmail.com>
2021-08-01 19:26:42 +0200

    Update references and docstring of `__init__.py`
    * Copyright text and docstring added to the beginning of the file
    * Missing references for `BaseInstrument` and `DeviceTypes` are added
    * Importing of instrument drivers made explicit to make it more clear
    and readable

--- src/zhinst/toolkit/control/drivers/__init__.py
+++ src/zhinst/toolkit/control/drivers/__init__.py
@@ -1,3 +1,13 @@
+# Copyright (C) 2020 Zurich Instruments
+#
+# This software may be modified and distributed under the terms
+# of the MIT license. See the LICENSE file for details.
+"""The Zurich Instruments Toolkit (zhinst-toolkit) Drivers
+
+This package is a collection of high level controllers for Zurich
+Instruments devices.
+"""
+from .base import BaseInstrument
 from .hdawg import HDAWG
 from .uhfqa import UHFQA
 from .uhfli import UHFLI
