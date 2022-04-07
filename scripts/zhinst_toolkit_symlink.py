71b328fd8e3df5cd04b28dad96553c3d562b3e7b
Markus Heikkinen <markus.heikkinen@zhinst.com>
2022-04-07 15:47:27 +0200

    Add external script for symlink

---
+++ scripts/zhinst_toolkit_symlink.py
@@ -0,0 +1,36 @@
+"""A helper script for developers to create symbolic link between existing toolkit and
+Python environment directories.
+
+By running the script, zhinst-toolkit is added to the ``zhinst`` package namespace
+for the given Python environment.
+
+Windows: Requires administration privileges.
+
+zhinst-toolkit must be installed before running the script.
+
+Running the script:
+
+    >>> python scripts\\zhinst_toolkit_symlink.py
+"""
+import os
+import sysconfig
+from pathlib import Path
+
+
+SRC_DIR = Path(Path(__file__).parent.parent / Path("src/zhinst/toolkit"))
+DEST_DIR = Path(sysconfig.get_path('purelib')) / "zhinst/toolkit"
+
+def create_symlink(src: Path, dest: Path) -> None:
+    """Create symbolic link between existing toolkit and
+    Python environment directories.
+    """
+    try:
+        # Windows: Requires administrator when running tests without symlink
+        # Otherwise Symlink must be made manually to add toolkit to Python site-packages
+        os.symlink(src, dest)
+        print(f"Symlink created. Source: {src}, Destination: {dest}")
+    except FileExistsError:
+        print(f"Destination: {dest} already exists.")
+
+if __name__ == "__main__":
+    create_symlink(SRC_DIR, DEST_DIR)
