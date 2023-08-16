zhinst/zhinst-toolkit

commit 0e1bc9224096b14a7dcaea643d91f3d7b251c55d

Fix some rare failure of wait_for_state_change.
In some rare cases, the function wait_for_state_change exited
early with a timeout error. That happened when a stale cached
value trigger an early detection of the state change. To avoid that,
a deep get is perfomed at the beginning and the logic is simplified.

Tests have been adapted to work with deep gets and to check the absence
of the previous faulty behaviour.

src/zhinst/toolkit/nodetree/node.py
@@ -946,21 +946,31 @@ class Node:
         else:
             value = self._parse_get_value(value)
             start_time = time.time()
-            while (
-                start_time + timeout >= time.time() and (self._get() == value) == invert
-            ):
+
+            # Performs a deep get to avoid waiting on stale values from cache
+            # In the loop we can use a shallow get
+            # Since deep get
+            curr_value = self._get(deep=True)[1]
+
+            while start_time + timeout >= time.time():
+                # Verify if we get to the correct value.
+                # If yes, exit the function.
+                if (curr_value == value) is not invert:
+                    return
+
                 time.sleep(sleep_time)
-            curr_value = self._get()
-            if (curr_value == value) is invert:
-                if invert:
-                    raise TimeoutError(
-                        f"{self.node_info.path} did not change from the expected"
-                        f" value {value} within {timeout}s."
-                    )
+                curr_value = self._get(deep=False)
+
+            # In case of timeout, raise the correct error
+            if invert:
                 raise TimeoutError(
-                    f"{self.node_info.path} did not change to the expected value"
-                    f" within {timeout}s. {value} != {curr_value}"
+                    f"{self.node_info.path} did not change from the expected"
+                    f" value {value} within {timeout}s."
                 )
+            raise TimeoutError(
+                f"{self.node_info.path} did not change to the expected value"
+                f" within {timeout}s. {value} != {curr_value}"
+            )
 
     def subscribe(self) -> None:
         """Subscribe to this node (its child lead nodes).

tests/test_nodetree.py
@@ -7,6 +7,7 @@ from unittest.mock import MagicMock, Mock
 from copy import deepcopy
 import pickle
 import gc
+from itertools import cycle
 
 import pytest
 from numpy import array as nparray
@@ -830,14 +831,23 @@ def test_subscribe(connection):
 def test_wait_for_state_change(connection):
     tree = NodeTree(connection, "DEV1234")
 
-    connection.getInt.side_effect = [1] * 3 + [2] * 2
+    sequence = iter([1] * 3 + [2] * 2)
+    connection.getInt.side_effect = lambda node: next(sequence)
+    connection.get.side_effect = lambda node, **kwargs: {
+        node: {"timestamp": [0], "value": [next(sequence)]}
+    }
     tree.demods[0].trigger.wait_for_state_change(2)
-    connection.getInt.side_effect = None
 
-    connection.getInt.return_value = 2
+    connection.getInt.side_effect = lambda node: 2
+    connection.get.side_effect = lambda node, **kwargs: {
+        node: {"timestamp": [0], "value": [2]}
+    }
     tree.demods[0].trigger.wait_for_state_change(2)
 
-    connection.getInt.return_value = 1
+    connection.getInt.side_effect = lambda node: 1
+    connection.get.side_effect = lambda node, **kwargs: {
+        node: {"timestamp": [0], "value": [1]}
+    }
     with pytest.raises(TimeoutError) as e:
         tree.demods[0].trigger.wait_for_state_change(2, timeout=0.5)
     assert (
@@ -852,7 +862,11 @@ def test_wait_for_state_change(connection):
         "expected value 1 within 0.5s."
     )
 
-    connection.getInt.side_effect = [1] * 3 + [2] * 8
+    sequence = cycle([1] * 3 + [2])
+    connection.getInt.side_effect = lambda node: next(sequence)
+    connection.get.side_effect = lambda node, **kwargs: {
+        node: {"timestamp": [0], "value": [next(sequence)]}
+    }
     tree.demods["*"].trigger.wait_for_state_change(2)
 
     with pytest.raises(KeyError) as e_info:
@@ -860,6 +874,30 @@ def test_wait_for_state_change(connection):
 
     with pytest.raises(KeyError) as e_info:
         tree.hello["*"].test.wait_for_state_change(1)
+
+    # Stale cache
+    sequence = iter([1] * 3 + [0])
+    stale_cache = True
+
+    def get_side_effect(node, **kwargs):
+        nonlocal stale_cache
+        stale_cache = False
+        return {node: {"timestamp": [0], "value": [next(sequence)]}}
+
+    def get_int_side_effect(node, **kwargs):
+        if stale_cache:
+            pytest.fail("wait_for_state_change accessed a stale cache element!")
+        return get_side_effect(node)[node]["value"][0]
+
+    connection.getInt.side_effect = get_int_side_effect
+    connection.get.side_effect = get_side_effect
+
+    tree.demods[0].trigger.wait_for_state_change(0)
+
+    # Verify the correct value has been fetched from the device
+    # and it didn't stop early.
+    with pytest.raises(StopIteration):
+        next(sequence)
 
 
 def test_nodetree_iterator(connection):

tests/test_awg.py
@@ -48,13 +48,17 @@ def test_wait_done(mock_connection, shfsg):
     single = 0
     enable = iter([])
 
-    def get_int_side_effect(node):
-        if node.upper() == "/DEV1234/SGCHANNELS/0/AWG/SINGLE":
-            return single
-        if node.upper() == "/DEV1234/SGCHANNELS/0/AWG/ENABLE":
-            return next(enable)
+    def get_side_effect(node, **kwargs):
+        if node.lower() == "/dev1234/sgchannels/0/awg/single":
+            return {node: {"timestamp": [0], "value": [single]}}
+        if node.lower() == "/dev1234/sgchannels/0/awg/enable":
+            return {node: {"timestamp": [0], "value": [next(enable)]}}
         raise RuntimeError("Node not found")
 
+    def get_int_side_effect(node):
+        return get_side_effect(node)[node]["value"][0]
+
+    mock_connection.return_value.get.side_effect = get_side_effect
     mock_connection.return_value.getInt.side_effect = get_int_side_effect
 
     # if not single mode this function throws a RuntimeError

tests/test_base.py
@@ -65,7 +65,12 @@ def test_factory_reset_ok(base_instrument, mock_connection):
 
 
 def test_factory_reset_timeout(base_instrument, mock_connection):
+    dev_id = base_instrument.serial.lower()
     mock_connection.return_value.getInt.return_value = 1
+    mock_connection.return_value.get.return_value = {
+        f"/{dev_id}/system/preset/busy": {"timestamp": [0], "value": [1]}
+    }
+
     with pytest.raises(TimeoutError):
         base_instrument.factory_reset(timeout=0.00001)
 

tests/test_device_settings_module.py
@@ -23,6 +23,10 @@ def test_repr(device_settings_module):
 
 def test_load_from_file(device_settings_module):
     device_settings_module.raw_module.getInt.return_value = 1
+    device_settings_module.raw_module.get.return_value = {
+        "/finished": {"timestamp": [0], "value": [1]}
+    }
+
     device_settings_module.load_from_file(Path("/path/to/file.json"), "dev1234")
     device_settings_module.raw_module.set.assert_any_call("/device", "dev1234")
     device_settings_module.raw_module.set.assert_any_call("/filename", "file")
@@ -33,6 +37,10 @@ def test_load_from_file(device_settings_module):
 
 def test_load_from_file_timeout(device_settings_module):
     device_settings_module.raw_module.getInt.return_value = 0
+    device_settings_module.raw_module.get.return_value = {
+        "/finished": {"timestamp": [0], "value": [0]}
+    }
+
     with pytest.raises(TimeoutError):
         device_settings_module.load_from_file(
             Path("/path/to/file.json"), "dev1234", timeout=0.01
@@ -41,6 +49,10 @@ def test_load_from_file_timeout(device_settings_module):
 
 def test_save_to_file(device_settings_module):
     device_settings_module.raw_module.getInt.return_value = 1
+    device_settings_module.raw_module.get.return_value = {
+        "/finished": {"timestamp": [0], "value": [1]}
+    }
+
     device_settings_module.save_to_file(Path("/path/to/file.json"), "dev1234")
     device_settings_module.raw_module.set.assert_any_call("/device", "dev1234")
     device_settings_module.raw_module.set.assert_any_call("/filename", "file")
@@ -51,6 +63,10 @@ def test_save_to_file(device_settings_module):
 
 def test_save_to_file_timeout(device_settings_module):
     device_settings_module.raw_module.getInt.return_value = 0
+    device_settings_module.raw_module.get.return_value = {
+        "/finished": {"timestamp": [0], "value": [0]}
+    }
+
     with pytest.raises(TimeoutError):
         device_settings_module.save_to_file(
             Path("/path/to/file.json"), "dev1234", timeout=0.01

tests/test_generator.py
@@ -34,13 +34,17 @@ def test_wait_done(mock_connection, shfqa, generator):
     single = 0
     enable = iter([])
 
-    def get_int_side_effect(node):
-        if node.upper() == "/DEV1234/QACHANNELS/0/GENERATOR/SINGLE":
-            return single
-        if node.upper() == "/DEV1234/QACHANNELS/0/GENERATOR/ENABLE":
-            return next(enable)
+    def get_side_effect(node, **kwargs):
+        if node.lower() == "/dev1234/qachannels/0/generator/single":
+            return {node: {"timestamp": [0], "value": [single]}}
+        if node.lower() == "/dev1234/qachannels/0/generator/enable":
+            return {node: {"timestamp": [0], "value": [next(enable)]}}
         raise RuntimeError("Node not found")
 
+    def get_int_side_effect(node):
+        return get_side_effect(node)[node]["value"][0]
+
+    mock_connection.return_value.get.side_effect = get_side_effect
     mock_connection.return_value.getInt.side_effect = get_int_side_effect
 
     # if not single mode this function throws a RuntimeError

tests/test_pqsc.py
@@ -93,12 +93,18 @@ def test_run_stop(mock_connection, pqsc):
 
 
 def test_wait_done(mock_connection, pqsc):
-    mock_connection.return_value.getInt.side_effect = [1, 1, 1, 0, 0, 0, 0, 0]
+    sequence = iter([1] * 3 + [0] * 5)
+    mock_connection.return_value.getInt.side_effect = lambda node: next(sequence)
+    mock_connection.return_value.get.side_effect = lambda node, **kwargs: {
+        node: {"timestamp": [0], "value": [next(sequence)]}
+    }
     pqsc.wait_done(sleep_time=0.001)
     mock_connection.return_value.getInt.assert_called_with("/dev1234/execution/enable")
 
-    mock_connection.return_value.getInt.side_effect = None
-    mock_connection.return_value.getInt.return_value = 1
+    mock_connection.return_value.getInt.side_effect = lambda node: 1
+    mock_connection.return_value.get.side_effect = lambda node, **kwargs: {
+        node: {"timestamp": [0], "value": [1]}
+    }
     with pytest.raises(TimeoutError) as e_info:
         pqsc.wait_done(timeout=0.01)
     mock_connection.return_value.getInt.assert_called_with("/dev1234/execution/enable")
@@ -110,7 +116,7 @@ def test_ref_clock(mock_connection, pqsc):
     source = 0
     source_actual = 0
 
-    def get_side_effect(path):
+    def getInt_side_effect(path):
         if path == "/dev1234/system/clocks/referenceclock/in/status":
             return next(status)
         if path == "/dev1234/system/clocks/referenceclock/in/source":
@@ -119,7 +125,12 @@ def test_ref_clock(mock_connection, pqsc):
             return source_actual
         raise RuntimeError("Invalid Node")
 
-    mock_connection.return_value.getInt.side_effect = get_side_effect
+    def get_side_effect(path, **kwargs):
+        value = getInt_side_effect(path)
+        return {path: {"timestamp": [0], "value": [value]}}
+
+    mock_connection.return_value.getInt.side_effect = getInt_side_effect
+    mock_connection.return_value.get.side_effect = get_side_effect
 
     assert pqsc.check_ref_clock(sleep_time=0.001)
     # Locked within time
@@ -159,7 +170,12 @@ def test_check_zsync_connection(mock_connection, pqsc):
             return status10
         raise RuntimeError("Invalid Node")
 
+    def get_side_effect(path, **kwargs):
+        value = getInt_side_effect(path)
+        return {path: {"timestamp": [0], "value": [value]}}
+
     mock_connection.return_value.getInt.side_effect = getInt_side_effect
+    mock_connection.return_value.get.side_effect = get_side_effect
 
     # not connected
     with pytest.raises(TimeoutError):

tests/test_readout.py
@@ -49,24 +49,38 @@ def test_run(mock_connection, readout):
 
 
 def test_stop(mock_connection, readout):
+    node = "/dev1234/qachannels/0/readout/result/enable"
+
     # already disabled
     mock_connection.return_value.getInt.return_value = 0
+    mock_connection.return_value.get.return_value = {
+        node: {"timestamp": [0], "value": [0]}
+    }
     readout.stop()
-    mock_connection.return_value.set.assert_called_with(
-        "/dev1234/qachannels/0/readout/result/enable", False
-    )
+    mock_connection.return_value.set.assert_called_with(node, False)
     # never disabled
     mock_connection.return_value.getInt.return_value = 1
+    mock_connection.return_value.get.return_value = {
+        node: {"timestamp": [0], "value": [1]}
+    }
     with pytest.raises(TimeoutError) as e_info:
         readout.stop(timeout=0.5)
 
 
 def test_wait_done(mock_connection, readout):
+    node = "/dev1234/qachannels/0/readout/result/enable"
+
     # already disabled
     mock_connection.return_value.getInt.return_value = 0
+    mock_connection.return_value.get.return_value = {
+        node: {"timestamp": [0], "value": [0]}
+    }
     readout.wait_done()
     # never disabled
     mock_connection.return_value.getInt.return_value = 1
+    mock_connection.return_value.get.return_value = {
+        node: {"timestamp": [0], "value": [1]}
+    }
     with pytest.raises(TimeoutError) as e_info:
         readout.wait_done(timeout=0.5)
 

commit 92b7b403d2ea8bd2258f6102a32470307666bf48

Fix wait_for_state change.
It was inadvertitly broken in 0e1bc9224096b14a7dcaea643d91f3d7b251c55d
The issue is that the zhinst core api sometimes return Python int (getInt),
and sometimes arrays of numpy int (get). As consequences, the result of a
comparison can be a numpy bool instead of bool, at that confused the
operator is, which check the equivalence of the objects, rather than
their value. Changed to a correct !=

Added a test case.

src/zhinst/toolkit/nodetree/node.py
@@ -977,7 +977,7 @@ class Node:
             while start_time + timeout >= time.time():
                 # Verify if we get to the correct value.
                 # If yes, exit the function.
-                if (curr_value == parsed_value) is not invert:
+                if (curr_value == parsed_value) != invert:
                     return
 
                 time.sleep(sleep_time)

tests/test_nodetree.py
@@ -970,6 +970,26 @@ def test_wait_for_state_change(connection):
     with pytest.raises(KeyError) as e:
         tree.demods[0].trigger.wait_for_state_change("non_existant_key")
 
+    # Numpy types
+    sequence = iter([1] * 3 + [0])
+
+    def get_side_effect(node, **kwargs):
+        val = nparray([next(sequence)], dtype="longlong")
+        return {node: {"timestamp": [0], "value": val}}
+
+    def get_int_side_effect(node, **kwargs):
+        return int(get_side_effect(node)[node]["value"][0])
+
+    connection.getInt.side_effect = get_int_side_effect
+    connection.get.side_effect = get_side_effect
+
+    tree.demods[0].harmonic.wait_for_state_change(0)
+
+    # Verify the correct value has been fetched from the device
+    # and it didn't stop early.
+    with pytest.raises(StopIteration):
+        next(sequence)
+
 
 def test_nodetree_iterator(connection):
     tree = NodeTree(connection, "DEV1234")