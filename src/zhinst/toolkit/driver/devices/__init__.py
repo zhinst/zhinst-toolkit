f3f0b01cbb30717fd9a079d0b3ac6746a05559a9
Andrea Corna <andrea.corna@zhinst.com>
2024-10-09 11:04:09 +0200

    Add support for QHub

--- src/zhinst/toolkit/driver/devices/__init__.py
+++ src/zhinst/toolkit/driver/devices/__init__.py
@@ -4,6 +4,7 @@ import typing as t
 from zhinst.toolkit.driver.devices.base import BaseInstrument
 from zhinst.toolkit.driver.devices.hdawg import HDAWG
 from zhinst.toolkit.driver.devices.pqsc import PQSC
+from zhinst.toolkit.driver.devices.qhub import QHub
 from zhinst.toolkit.driver.devices.shfqa import SHFQA
 from zhinst.toolkit.driver.devices.shfsg import SHFSG
 from zhinst.toolkit.driver.devices.uhfli import UHFLI
@@ -11,7 +12,9 @@ from zhinst.toolkit.driver.devices.uhfqa import UHFQA
 
 from zhinst.toolkit.driver.devices.shfqc import SHFQC  # isort:skip
 
-DeviceType = t.Union[BaseInstrument, HDAWG, PQSC, SHFQA, SHFSG, UHFLI, UHFQA, SHFQC]
+DeviceType = t.Union[
+    BaseInstrument, HDAWG, PQSC, QHub, SHFQA, SHFSG, UHFLI, UHFQA, SHFQC
+]
 
 DEVICE_CLASS_BY_MODEL = {
     "SHFQC": SHFQC,
@@ -22,6 +25,7 @@ DEVICE_CLASS_BY_MODEL = {
     "HDAWG4": HDAWG,
     "HDAWG8": HDAWG,
     "PQSC": PQSC,
+    "QHUB": QHub,
     "UHFQA": UHFQA,
     "UHFLI": UHFLI,
     "UHFAWG": UHFLI,
@@ -33,6 +37,7 @@ __all__ = [
     "BaseInstrument",
     "HDAWG",
     "PQSC",
+    "QHub",
     "SHFQA",
     "SHFSG",
     "UHFLI",
--- src/zhinst/toolkit/driver/devices/qhub.py
+++ src/zhinst/toolkit/driver/devices/qhub.py
@@ -1,29 +1,26 @@
-"""PQSC Instrument Driver."""
+"""QHub Instrument Driver."""
 
 from zhinst.toolkit.driver.devices.quantum_system_hub import QuantumSystemHub
 
 
-class PQSC(QuantumSystemHub):
-    """High-level driver for the Zurich Instruments PQSC."""
+class QHub(QuantumSystemHub):
+    """High-level driver for the Zurich Instruments QHub."""
 
     def arm(self, *, deep=True, repetitions: int = None, holdoff: float = None) -> None:
-        """Prepare PQSC for triggering the instruments.
+        """Prepare QHub for triggering the instruments.
 
-        This method configures the execution engine of the PQSC and
-        clears the register bank. Optionally, the *number of triggers*
+        This method configures the execution engine of QHub.
+        Optionally, the *number of triggers*
         and *hold-off time* can be set when specified as keyword
         arguments. If they are not specified, they are not changed.
 
-        Note that the PQSC is disabled at the end of the hold-off time
-        after sending out the last trigger. Therefore, the hold-off time
-        should be long enough such that the PQSC is still enabled when
-        the feedback arrives. Otherwise, the feedback cannot be processed.
+        Note that the QHub is disabled at the end of the hold-off time
+        after sending out the last trigger.
 
         Args:
             deep: A flag that specifies if a synchronization
                 should be performed between the device and the data
-                server after stopping the PQSC and clearing the
-                register bank (default: True).
+                server after stopping QHub (default: True).
             repetitions: If specified, the number of triggers sent
                 over ZSync ports will be set (default: None).
             holdoff: If specified, the time between repeated
@@ -32,11 +29,9 @@ class PQSC(QuantumSystemHub):
                 (default: None).
 
         """
-        # Stop the PQSC if it is already running
+        # Stop QHub if it is already running
         self.stop(deep=deep)
         if repetitions is not None:
             self.execution.repetitions(repetitions)
         if holdoff is not None:
             self.execution.holdoff(holdoff)
-        # Clear register bank
-        self.feedback.registerbank.reset(1, deep=deep)
